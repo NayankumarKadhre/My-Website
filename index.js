e36f757e347a136d866588a50a36982c57b14e74
Nayan Kadhre <nayankumarkadhre@gmail.com>
2023-12-30 03:42:01 +0530

    update website

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ window.onscroll = function () {
   scrollFunction();
 };
 
-function scrollFunction(){
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
-  } 
-  else{
+  } else {
     mybutton.style.display = "none";
   }
 }
@@ -19,3 +18,12 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the current year
+  var currentYear = new Date().getFullYear();
+
+  // Set the year in the footer
+  document.getElementById("year").textContent =
+    "© " + currentYear + " Nayan Kadhre.";
+});
